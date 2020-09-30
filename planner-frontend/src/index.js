bfc828e1f467d79432071eb0fae5f89f70286038
hansenjl <jenn.leigh.hansen@gmail.com>
2020-09-29 23:55:48 -0700

    connect to the redux store and Router

--- planner-frontend/src/index.js
+++ planner-frontend/src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import userReducer from "./reducers/userReducer";
+import tripReducer from "./reducers/tripReducer";
+
+const store = createStore(
+  combineReducers({ user: userReducer, trips: tripReducer }),
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
