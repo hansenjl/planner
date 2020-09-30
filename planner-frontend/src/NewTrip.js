5868870711f49f2ee6df6a180c1de61152e37c75
hansenjl <jenn.leigh.hansen@gmail.com>
2020-09-29 23:56:51 -0700

    template for new trip form component

---
+++ planner-frontend/src/NewTrip.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+// import { createTrip } from "./actions/tripActions";
+
+const NewTrip = (props) => {
+  return (
+    <div>
+      <h2>New Trip Page </h2>
+    </div>
+  );
+};
+
+export default NewTrip;
