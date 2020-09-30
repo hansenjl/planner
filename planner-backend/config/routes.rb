3295b7cfd401a04c6cbf640fa7283ba282f1511a
hansenjl <jenn.leigh.hansen@gmail.com>
2020-09-29 23:53:40 -0700

    add nested routes for trips

--- planner-backend/config/routes.rb
+++ planner-backend/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :activities
+  resources :timeslots
+  resources :trips
+  resources :users, only: [:create, :show, :index] do
+    resources :trips, only: [:create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
